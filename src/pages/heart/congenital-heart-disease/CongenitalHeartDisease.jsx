4baf5737bdc55c57563dae94a028dc12aa90cede
c3s4rXD <cesar.hincapie@correounivalle.edu.co>
2025-04-22 10:13:40 -0500

    organizacion del layout e index css y carga de primer modelo

--- src/pages/heart/congenital-heart-disease/CongenitalHeartDisease.jsx
+++ src/pages/heart/congenital-heart-disease/CongenitalHeartDisease.jsx
@@ -1,10 +1,25 @@
 import "./CongenitalHeartDisease.css";
+import GLBViewer from "../../../components/GLBViewer";
+import React from 'react';
 
 const CongenitalHeartDisease = () => {
-  return <div> Cardiopatia Congenita </div>;
+
+  return(
+    <div style={{ height: '100vh', width: '100vw' }}>
+        <p>Cardiopatia congenita</p>
+        {/*Muchachos es para acomodar la posicion de la camara 
+        cameraPosition{x,y,z} y fov es para el zoom de la camara
+        como se que cada modelo es distinto lo deje dinamico */}
+
+        <GLBViewer modelUrl="../../../../public/3Dmodels/congenital-heart-disease-models/Heart.glb" 
+         cameraPosition={[0, 1.5, 4]} fov={20} />
+    </div>
+  )
 };
 
 export default CongenitalHeartDisease;
 
-//cesar
-//cesar prueba develop
+
+
+
+
