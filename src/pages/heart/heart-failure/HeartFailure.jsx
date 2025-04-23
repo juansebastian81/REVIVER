3e9c368df0d064eefbfe2aaea8ccc48bed0bfc2b
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-04-23 01:28:36 -0500

    Se eliminaron archivos sin usar y se corrigieron nombres de archivos y carpetas

--- src/pages/heart/heart-failure/HeartFailure.jsx
+++ src/pages/heart/heart-failure/HeartFailure.jsx
@@ -1,21 +1,22 @@
 import "./HeartFailure.css";
 import GLBViewer from "../../../components/GLBViewer";
-import React from 'react';
+import React from "react";
 
 const HeartFailure = () => {
-
-  return(
-    <div style={{ height: '100vh', width: '100vw' }}>
-        <p>Sintoma: Confusion</p>
-        {/*Muchachos es para acomodar la posicion de la camara 
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <p>Sintoma: Confusion</p>
+      {/*Muchachos es para acomodar la posicion de la camara 
         cameraPosition{x,y,z} y fov es para el zoom de la camara
         como se que cada modelo es distinto lo deje dinamico */}
 
-        <GLBViewer modelUrl="../../../../public/3Dmodels/heart-failure-models/Confusion.glb" 
-         cameraPosition={[0, 1.5, 4]} fov={20} />
+      <GLBViewer
+        modelUrl="../../../../public/models-3d/heart-failure/Confusion.glb"
+        cameraPosition={[0, 1.5, 4]}
+        fov={20}
+      />
     </div>
-  )
+  );
 };
 
 export default HeartFailure;
-
