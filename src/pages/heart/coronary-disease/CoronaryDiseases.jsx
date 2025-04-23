bccafed4a2b57d237bce2f3470c09841aacda339
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-04-22 22:49:05 -0500

    Se crean nuevas carpetas y se suben modelos 3d

---
+++ src/pages/heart/coronary-disease/CoronaryDiseases.jsx
@@ -0,0 +1,7 @@
+import "./CoronaryDiseases.css";
+
+const CoronaryDiseases = () => {
+  return <div> Enfermedad coronaria </div>;
+};
+
+export default CoronaryDiseases;
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,17 +1,15 @@
 import "./Layout.css";
-import Footer from "./footer/Footer"; 
+import Footer from "./footer/Footer";
 import Header from "./header/Header";
 
 const Layout = ({ children }) => {
-
-    return (
-        <div className="layout">
-            <Header />
-            <main className="layout-content">{ children }</main>
-            <Footer />
-        </div>
-    );
-    
+  return (
+    <div className="layout">
+      <Header />
+      <main className="layout-content">{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
