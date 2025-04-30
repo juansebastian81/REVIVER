8ef6271e0dfbc47d50ea84f580513ba752d4c963
YeimerrrM <yeimer.mendez@correounivalle.edu.co>
2025-04-30 15:17:35 -0500

    Fix Conflicts Arritmia Section

--- src/pages/heart/arrhythmia/Arrhythmia.jsx
+++ src/pages/heart/arrhythmia/Arrhythmia.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import GLBViewer from "../../../components/GLBViewer";
-import React from "react";
-import GLBViewer from "../../../components/GLBViewer";
 import "./Arrhythmia.css";
 
 const Arrhythmia = () => {
