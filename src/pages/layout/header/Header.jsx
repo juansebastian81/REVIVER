5f452af03bd8b9c5074b50b9a3f354414004d219
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-05-11 19:15:57 -0500

    Se añade menú para los modelos de forma preventiva

--- src/pages/layout/header/Header.jsx
+++ src/pages/layout/header/Header.jsx
@@ -1,21 +1,52 @@
 import "./Header.css";
+import { useState } from "react";
 import { NavLink } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const openCloseDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
   return (
     <header>
       <nav>
         <div className="logo-container">
           <a href="/">
-          <img className="logo" src="/logo/logoNuevoSinFondoRecortado.png" />
+            <img className="logo" src="/logo/logoNuevoSinFondoRecortado.png" />
           </a>
         </div>
         <NavLink to="/" end>
           Inicio
         </NavLink>{" "}
         &nbsp;
-        <NavLink to="/models" end>
-          Corazones
+        <NavLink>
+          <Dropdown isOpen={dropdown} toggle={openCloseDropdown}>
+            <DropdownToggle caret>Corazones</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Enfermedades</DropdownItem>
+              <NavLink to="/models/arrhythmia" end>
+                <DropdownItem>Arritmia</DropdownItem>
+              </NavLink>
+              <NavLink to="/models/heart-failure" end>
+                <DropdownItem>Insuficiencia Cardiaca</DropdownItem>
+              </NavLink>
+              <NavLink to="/models/coronary-disease" end>
+                <DropdownItem>Enfermedad Coronaria</DropdownItem>
+              </NavLink>
+              <NavLink to="/models/congenital-heart-disease" end>
+                <DropdownItem>Cardiopatia Congenita</DropdownItem>
+              </NavLink>
+            </DropdownMenu>
+          </Dropdown>
         </NavLink>{" "}
         &nbsp;
         <NavLink to="/quiz" end>
