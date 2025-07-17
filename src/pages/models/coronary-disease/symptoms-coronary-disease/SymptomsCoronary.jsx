b6f77841d43ead7fbc13fbcca4ef352b74b247ea
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-07-16 21:42:25 -0500

    Se ajusta staging

--- src/pages/models/coronary-disease/symptoms-coronary-disease/SymptomsCoronary.jsx
+++ src/pages/models/coronary-disease/symptoms-coronary-disease/SymptomsCoronary.jsx
@@ -36,14 +36,16 @@ const SymptomsCoronary = () => {
       <BreadCrumbs />
       <div className="viewer-container">
         <GLBViewer
+          stagingModel="park"
           modelUrl="/models-3d/coronary-disease/AvatarJSOM.glb"
           scaleModel={[0.1, 0.1, 0.1]}
-          positionModel={[0, -0.1, 0]}
-          cameraPosition={[0, 0, 5]}
-          fov={4}
+          positionModel={[0, 0.001, 0]}
+          cameraPosition={[0, 0.5, 4.5]}
+          targetModel={[0, 0.05, 0]}
+          fov={5}
           titleHeart="Sintomas"
           titleSize={0.04}
-          titlePosition={[0, 0.125, -0.1]}
+          titlePosition={[0, 0.21, -0.1]}
           shadowPosition={[0, -0.1, 0]}
           defaultAnimation="Look"
           animationMap={{
