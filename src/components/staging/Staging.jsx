e8dfeb05d575a76bdea77683b4207a47d3e258eb
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-07-22 14:16:05 -0500

    Se corrige staging ya actualiza de forma automatica

--- src/components/staging/Staging.jsx
+++ src/components/staging/Staging.jsx
@@ -1,16 +1,42 @@
 import { Environment } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+import { CubeTextureLoader } from "three";
 
 const Staging = ({ environmentName }) => {
+  const { scene } = useThree();
+
+  useEffect(() => {
+    const loader = new CubeTextureLoader();
+    loader.setPath(`/staging/cubemaps/${environmentName}/`);
+
+    loader.load(
+      ["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"],
+      (texture) => {
+        scene.background = texture;
+        scene.environment = texture;
+      }
+    );
+
+    return () => {
+      // Limpieza previa para evitar quedarse con la anterior
+      scene.background = null;
+      scene.environment = null;
+    };
+  }, [environmentName, scene]);
+
   return (
     <Environment
-      path={`/staging/cubemaps/${environmentName}/`}
-      files={["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]}
-      ground={{
-        height: 0.15,
-        radius: 1,
-        scale: 100,
-      }}
-      background
+      // No se define `background` para no sobrescribir lo que hicimos manualmente
+      files={[
+        `/staging/cubemaps/${environmentName}/px.png`,
+        `/staging/cubemaps/${environmentName}/nx.png`,
+        `/staging/cubemaps/${environmentName}/py.png`,
+        `/staging/cubemaps/${environmentName}/ny.png`,
+        `/staging/cubemaps/${environmentName}/pz.png`,
+        `/staging/cubemaps/${environmentName}/nz.png`,
+      ]}
+      ground={{ height: 0.15, radius: 1, scale: 100 }}
     />
   );
 };
--- src/pages/models/coronary-disease/symptoms-coronary-disease/SymptomsCoronary.jsx
+++ src/pages/models/coronary-disease/symptoms-coronary-disease/SymptomsCoronary.jsx
@@ -39,12 +39,12 @@ const SymptomsCoronary = () => {
           scaleModel={[0.1, 0.1, 0.1]}
           positionModel={[0, 0.001, 0]}
           cameraPosition={[0, 0.5, 4.5]}
-          targetModel={[0, 0.05, 0]}
+          targetModel={[0, 0.09, 0]}
           fov={5}
           titleHeart="Sintomas"
           titleSize={0.04}
           titlePosition={[0, 0.21, -0.1]}
-          shadowPosition={[0, -0.1, 0]}
+          shadowPosition={[0, -0.01, 0]}
           defaultAnimation="Look"
           animationMap={{
             KeyQ: "Dizzy",
