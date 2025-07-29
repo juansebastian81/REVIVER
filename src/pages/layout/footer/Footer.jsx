f05982ed036dda14646621366f0e2492c68733a1
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-07-28 19:32:36 -0500

    Se añade el mapa de sitio y se corrige las migas de pan

--- src/pages/layout/footer/Footer.jsx
+++ src/pages/layout/footer/Footer.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer>
+      <li>
+        <Link to="/sitemap">Mapa del sitio</Link>
+      </li>
       <p className="footer">&copy; REVIVER 2025</p>
     </footer>
   );
---
+++ src/pages/site/SiteMap.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SiteMap.css";
+
+const SiteMap = () => {
+  return (
+    <div className="sitemap-container">
+      <h1>Mapa del Sitio</h1>
+      <ul className="sitemap-list">
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/diseases">Enfermedades</Link>
+          <ul>
+            <li>
+              <Link to="/diseases/arrhythmia">Arritmia</Link>
+              <ul>
+                <li>
+                  <Link to="/diseases/arrhythmia/symptoms-arrhythmia">
+                    Sintomas
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/diseases/arrhythmia/treatment-arrhythmia">
+                    Tratamiento
+                  </Link>
+                </li>
+                <li>
+                  <Link to="">Prevención</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/diseases/heart-failure">Insuficiencia Cardiaca</Link>
+              <ul>
+                <li>
+                  <Link to="/diseases/heart-failure/fatigue-symptom">
+                    Sintomas
+                  </Link>
+                </li>
+                <li>
+                  <Link to="">Tratamiento</Link>
+                </li>
+                <li>
+                  <Link to="">Prevención</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/diseases/coronary-disease">Enfermedad Coronaria</Link>
+              <ul>
+                <li>
+                  <Link to="/diseases/coronary-disease/symptoms-coronary-disease">
+                    Sintomas
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/diseases/coronary-disease/treatment-coronary-disease">
+                    Tratamiento
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/diseases/coronary-disease/prevention-care-coronary-disease">
+                    Prevención
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/diseases/congenital-heart-disease">
+                Cardiopatía Congénita
+              </Link>
+              <ul>
+                <li>
+                  <Link to="/diseases/congenital-heart-disease/fatigue">
+                    Sintomas
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/diseases/congenital-heart-disease/treatment-congenital">
+                    Tratamiento
+                  </Link>
+                </li>
+                <li>
+                  <Link to="">Prevención</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/quiz">Quiz interactivo</Link>
+        </li>
+        <li>
+          <Link to="/aboutus">Sobre Nosotros</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SiteMap;
