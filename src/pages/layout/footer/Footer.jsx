8b5049753682a53b3ff63d6f888b57460e8036c2
Juan Sebastian Ospina Maya <juan.sebastian.ospina@correounivalle.edu.co>
2025-07-28 20:01:03 -0500

    Se modifica el footer a uno mas bonito

--- src/pages/layout/footer/Footer.jsx
+++ src/pages/layout/footer/Footer.jsx
@@ -4,10 +4,44 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <li>
-        <Link to="/sitemap">Mapa del sitio</Link>
-      </li>
-      <p className="footer">&copy; REVIVER 2025</p>
+      <ul>
+        <li>
+          <Link to="/sitemap">Mapa del sitio</Link>
+        </li>
+        <li>
+          <Link to="/aboutus">Sobre nosotros</Link>
+        </li>
+        <li>
+          <Link to="/aboutus">Contáctanos</Link>
+        </li>
+      </ul>
+
+      <div className="footer-info">
+        <p>
+          📍 Dirección:{" "}
+          <a href="https://maps.app.goo.gl/i5KEs3TkmwzNh2326" target="_blank">
+            Campus Meléndez Calle 13 # 100-00, Santiago de Cali, Valle del
+            Cauca, Colombia
+          </a>
+        </p>
+        <p>
+          📞 Teléfono:{" "}
+          <a
+            href="https://wa.me/573243819539?text=Hola,%20quiero%20más%20información%20sobre%20REVIVER"
+            target="_blank"
+          >
+            (+57) 324-3819539{" "}
+          </a>
+        </p>
+        <p>
+          ✉️ Correo:{" "}
+          <a href="mailto:contacto@reviver.com">contacto@reviver.com</a>
+        </p>
+      </div>
+
+      <p className="footer">
+        &copy; REVIVER 2025 - Todos los derechos reservados
+      </p>
     </footer>
   );
 };
